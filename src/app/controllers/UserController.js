f7292cf5af786684c7336610a721a83a5ca61714
valdir-ti <valdir.ti@gmail.com>
2020-01-20 17:06:57 -0300

    Verificação se o usuário já está cadastrado no sistema

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,9 +2,13 @@ import User from '../models/Users';
 
 class UserController {
   async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(401).json({ message: 'User already exists.' });
+    }
+
     const user = await User.create(req.body);
-
-
     return res.json(user);
   }
 }
