0d2f2ac9671582f48e10949a3ab2671c60bf85c2
valdir-ti <valdir.ti@gmail.com>
2020-01-20 15:16:47 -0300

    Migration create users

--- src/database/migrations/20200120175039-create-users.js
+++ src/database/migrations/20200120175039-create-users.js
@@ -1,11 +1,41 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    
-  },
+  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {
 
-  down: (queryInterface, Sequelize) => {
-    
-  }
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    register: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    }
+
+
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('users')
 };
