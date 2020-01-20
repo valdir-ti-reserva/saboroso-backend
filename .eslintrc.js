89acedba4e7a07e113a095bae44a969bc21fd56b
valdir-ti <valdir.ti@gmail.com>
2020-01-20 14:52:52 -0300

    Sequelize, prettier e eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "class-methods-use-this": "off",
+        "no-param-reassign": "off",
+        "camelcase": "off",
+        "no-unused-vars": ["error", { "argsIgnorePattern": "next"}]
+    }
+};
