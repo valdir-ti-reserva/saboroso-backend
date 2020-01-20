8741c841aca89ae886906f45bd02fdeb3835d870
valdir-ti <valdir.ti@gmail.com>
2020-01-20 15:59:44 -0300

    Ajustes nas falhas de dependênicas apontadas no sistema

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,23 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
+    env: {
+      es6: true,
+      node: true,
     },
-    "extends": [
-        "airbnb-base"
+    extends: [
+      'airbnb-base',
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
     },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "class-methods-use-this": "off",
-        "no-param-reassign": "off",
-        "camelcase": "off",
-        "no-unused-vars": ["error", { "argsIgnorePattern": "next"}]
-    }
-};
+    rules: {
+      "class-methods-use-this":"off",
+      "no-param-reassign": "off",
+      "camelcase": "off",
+      "no-unused-vars": ["error", {"argsIgnorePattern": "next", "vars": "all", "args": "after-used", "ignoreRestSiblings": false}]
+    },
+  };
--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import express from 'express';
 
 const server = express();
 
-server.get('/teste', (req, res)=>{
-
-  return res.json({ message: 'OK' });
-  
-});
+server.get('/teste', (req, res) => res.json({ message: 'OK' }));
 
 server.listen(3333);
