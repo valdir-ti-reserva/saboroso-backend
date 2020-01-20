8bff4f45e3f1333efdc10a2344c193f885af3063
valdir-ti <valdir.ti@gmail.com>
2020-01-20 15:33:01 -0300

    Migration de menus

---
+++ src/database/migrations/20200120182906-create-menus.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('menus', {
+
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    price: {
+      type: Sequelize.FLOAT,
+      allowNull: false,
+    },
+    photo: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    register: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    }
+
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('menus')
+
+};
