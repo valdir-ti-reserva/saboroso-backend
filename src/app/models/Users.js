7233651b7e3d7e2ca44ca8b71431ce1cc270c180
valdir-ti <valdir.ti@gmail.com>
2020-01-20 17:44:28 -0300

    Gerando o hash da senha do usuário

--- src/app/models/Users.js
+++ src/app/models/Users.js
@@ -1,16 +1,27 @@
 import Sequelize, { Model } from 'sequelize';
-// import bcript from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
     super.init({
       name: Sequelize.STRING,
       email: Sequelize.STRING,
+      password_hash: Sequelize.VIRTUAL,
       password: Sequelize.STRING,
     },
     {
       sequelize,
     });
+
+    // Executa uma ação automática no Sequelize
+    // beforeSave executa antes de salvar os dados no banco
+    this.addHook('beforeSave', async (user) => {
+      if (user.password_hash) {
+        user.password = await bcrypt.hash(user.password_hash, 8);
+      }
+    });
+
+    return this;
   }
 }
 
